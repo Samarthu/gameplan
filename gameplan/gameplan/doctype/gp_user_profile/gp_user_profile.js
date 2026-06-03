554c6fc3e39f37b3dbedc357835268c1329f4958
TechnicalShree <krushnasr96@gmail.com>
2026-06-03 07:55:34 +0530

    feat: implement reporting structure in GP User Profile with reportee management

--- gameplan/gameplan/doctype/gp_user_profile/gp_user_profile.js
+++ gameplan/gameplan/doctype/gp_user_profile/gp_user_profile.js
@@ -1,7 +1,13 @@
 // Copyright (c) 2022, Frappe Technologies Pvt Ltd and contributors
 // For license information, please see license.txt
 
+const REPORTEE_USER_QUERY =
+  "gameplan.gameplan.doctype.gp_user_profile.gp_user_profile.reportee_user_query";
+
 frappe.ui.form.on("GP User Profile", {
-  // refresh: function(frm) {
-  // }
+  refresh: function (frm) {
+    frm.set_query("user", "reportees", () => {
+      return { query: REPORTEE_USER_QUERY };
+    });
+  },
 });
